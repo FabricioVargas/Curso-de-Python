bc2628c32774b7c49ed4effce5153be42eefd785
Fabricio Vargas Alvarez <fabriciovargas957@gmail.com>
2020-07-18 23:34:54 -0600

    Se agregaron modificaciones al archivo condicionales 2

--- condicionales2.py
+++ condicionales2.py
@@ -1,6 +1,23 @@
-edad=-7
+salarioPresidente=int(input("Ingrese el salario del Presidente de la empresa: "))
+salarioDirector=int(input("Ingrese el salario del directir de la empresa: "))
+salarioJefeAministrativo=int(input("Ingrese el salario del jefe administrativo de la empresa: "))
+salarioAministrativo=int(input("Ingrese el salario del administrativo de la empresa: "))
 
-if 0<edad<100:
-    print("Edad es correcta")
+print("El salario del jefe es :" + str(salarioPresidente))
+
+if salarioAministrativo<salarioJefeAministrativo<salarioDirector<salarioPresidente:
+    print("Los salarios fueron ingresados correcatmente")
 else:
-    print("Edad es incorrecta")
+    print("Hay diferencia entre los salarios")
+
+#funcion str() me permite convertir un int a estring para concatenarlo a un texto
+
+print("Asignaturas optativas")
+print("Informatica grafica - Pruebas de software - Usabilidad y accesibilidad")
+opcion=input("Introduce la asignatura escogida: ")
+
+asignatura=opcion.lower()
+if asignatura in ("informatica grafica", "pruebas de software", "usabilidad y accesibilidad"):
+    print("Asignatura eligida " + asignatura)
+else:
+    print("Asignatura no contemplada")
