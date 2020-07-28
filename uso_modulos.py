fc439545dbfa51c3d1cc22957bd5411a2a9293a1
Fabricio Vargas Alvarez <fabriciovargas957@gmail.com>
2020-07-28 10:30:35 -0600

    Se agrego el archivo uso_modulos.py

---
+++ uso_modulos.py
@@ -0,0 +1,6 @@
+#from calculos.calculos_generales import sumar # llamanos a el paquete de calculos fuera de esa carpeta
+
+from calculos.basicos.operaciones_basicas import * # estoy llamando a un sub carpeta de una carpeta fuera del archivo fuera de la raiz
+
+
+sumar(1,4)
