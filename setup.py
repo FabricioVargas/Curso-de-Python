19123bb6efd53b60a9de9a4e4bf145b1cccc3af8
Fabricio Vargas Alvarez <fabriciovargas957@gmail.com>
2020-07-28 10:31:48 -0600

    Se agrego el archivo de setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(#descripcion del paquetes
+    name="PaqueteCalculos",
+    version="1.0",
+    description="Paquete de redondeo y potencia",
+    author="Fabricio",
+    author_email="fabricio@fabricio.com",
+    url="www.yputube.com",
+    packages=["calculos","calculos.redondeo_potencia"]
+    )
